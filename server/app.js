563ba59f96b47dcb3135018e8dbec344da68f5ad
Richard Lorenzini <richardlorenzini@yahoo.com>
2019-06-16 16:18:08 -0400

    failed to load bcrypt. again. like always.

--- server/app.js
+++ server/app.js
@@ -1,1 +1,26 @@
-//app.js file for server 
+//app.js file for server
+const express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser'),
+  cors = require('cors'),
+  path = require('path'),
+  Sequelize = require('sequelize'),
+  Op = Sequelize.Op,
+  jwt = require('jsonwebtoken'),
+  // bcrypt = require('bcrypt'),
+  // SALT_ROUNDS = 10,
+  // myPlaintextPassword = 's0/\/\P4$$w0rD',
+  // someOtherPlaintextPassword = 'not_bacon',
+  // schedule = require('node-schedule'),
+  PORT = process.env.PORT || 8080;
+// Why does bcrypt never work for me?
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server running at localhost: ${PORT} `);
+});
