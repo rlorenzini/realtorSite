d8b7f6cfdee1c69c14f808663e65bc385526d7b2
Richard Lorenzini <richardlorenzini@yahoo.com>
2019-07-03 17:00:13 -0400

    stuff

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,17 @@
 import React,{Component} from 'react';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
+import './styling/Header.css';
 
 class BaseLayout extends Component {
   render(){
   return (
-    <div>
-      <h3>This is the header</h3>
+    <div className="headerMenu">
+    <ul>
+      <li>Home</li>
+      <li>Page 1</li>
+      <li>Page 2</li>
+      </ul>
     </div>
   )};
 }
